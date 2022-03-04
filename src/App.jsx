16337bd029278cb426c9be171dcd0d11932e3726
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-04 18:25:57 +0100

    styling for APP and SearchBar Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import {ShoppingList} from './Components/ShoppingList.jsx'
+import { useState } from 'react';
+import './App.css';
+import { ShoppingList } from './Components/ShoppingList.jsx';
 
 export function App() {
   return (
     <div className="App">
-      <header className="App_header">
-        <div className="Shopping_List"><ShoppingList/></div>
-      </header>
+      <div></div>
+      <div className="Shopping_List">
+        <ShoppingList />
+      </div>
     </div>
-  )
+  );
 }
-
-
--- src/Components/ShoppingList.jsx
+++ src/Components/ShoppingList.jsx
@@ -1,55 +1,45 @@
 import { useState, useEffect } from 'react';
-import './ShoppingList.css'
+import './ShoppingList.css';
 import { search } from 'fast-fuzzy';
 
-const url='https://fetch-me.vercel.app/api/shopping/items';
+const url = 'https://fetch-me.vercel.app/api/shopping/items';
 
-export function ShoppingList(){
-    const[items,setItems] = useState([]);
-    const[searchItems,setSearchItems] = useState([]);
+export function ShoppingList() {
+  const [items, setItems] = useState([]);
+  const [searchItems, setSearchItems] = useState([]);
 
-    useEffect(() =>{
-         async function fetchData (){
-            try{
-                const response = await fetch(url);
-                const data = await response.json();
-                console.log(data.data);
-                setItems(data.data);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.data);
+        setItems(data.data);
 
-                return(data.data);
-            }catch(error){
-                console.log(error);
-            }
-        }
-        fetchData();
-    },[])
-
-    
-    function fuzzySearch(event) {
-        console.log(event.target.value)
-        setSearchItems(search(
-        event.target.value,
-        items,
-        {keySelector: (obj) => obj.name.de},
-    ));
+        return data.data;
+      } catch (error) {
+        console.log(error);
+      }
     }
-    return(
-        <>
-        <input
-        type="text"
-        placeholder="search"
-        onChange={fuzzySearch}
-        />
-        <ul>
-        {searchItems.map(({ name, _id }) => (
-          <li key={_id}>{name.de}</li>
+    fetchData();
+  }, []);
+
+  function fuzzySearch(event) {
+    console.log(event.target.value);
+    setSearchItems(
+      search(event.target.value, items, { keySelector: obj => obj.name.de })
+    );
+  }
+  return (
+    <>
+      <div className="Search_Bar">
+        <input type="text" placeholder="search" onChange={fuzzySearch} />
+      </div>
+      <ul>
+        {searchItems.map(item => (
+          <li key={item._id}>{item.name.de}</li>
         ))}
-        </ul>
-        </>
-     )
+      </ul>
+    </>
+  );
 }
-
-
-
-
-
