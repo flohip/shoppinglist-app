6ca6a32bb5b99713ce3f5a949300dde03cf2ab66
Florian Hippenstiel <florian.hippenstiel@web.de>
2022-03-05 12:10:02 +0100

    added activeList and inactiveList

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import './variables.css';
+import './utils.css';
 import { ShoppingList } from './Components/ShoppingList.jsx';
 
 export function App() {
   return (
     <div className="App">
-      <body>
-        <div></div>
+      <body className="">
+        <div className="active_List"></div>
         <div className="shopping_List">
           <ShoppingList />
         </div>
+        <div className="inactive_List"></div>
       </body>
     </div>
   );
